d63dd52512fc63dd79d0578467e72c5aebb045f8
hunny <hunnyjain711@gmail.com>
2023-03-07 00:00:39 +0530

    23 passed

--- 23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists.py
@@ -10,12 +10,34 @@ class ListNode:
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummy = list1 = ListNode()
+        def mergeTwoList(left, right):
+            dummy = mergelist = ListNode()
+            while left and right:
+                if left.val < right.val:
+                    mergelist.next = left
+                    mergelist = mergelist.next
+                    left = left.next
+                else:
+                    mergelist.next = right
+                    mergelist = mergelist.next
+                    right = right.next
+            if left or right:
+                mergelist.next = left if left else right
+            return dummy.next
 
-        for lis in lists:
-            if list1 and lis and lis.next:
-                list1.next, list1 = lis, list1.next
-        return dummy
+        lists_len = len(lists)
+        if lists_len == 0:
+            return None
+        if lists_len == 1:
+            return lists[0]
+
+        new_head = mergeTwoList(lists[0], lists[1])
+
+        count = 2
+        while count < lists_len:
+            new_head = mergeTwoList(lists[count], new_head)
+            count += 1
+        return new_head
 
 
 if __name__ == '__main__':
