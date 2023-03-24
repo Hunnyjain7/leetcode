3b84ec6a4da25e50a51f84a49f443c64df1597ad
hunny <hunnyjain711@gmail.com>
2023-03-25 00:04:49 +0530

    40 Passed

--- 40. Combination Sum II.py
+++ 40. Combination Sum II.py
@@ -2,8 +2,34 @@ from typing import List
 
 
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        pass
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:  # noqa
+        currset, subsets = [], []
+        if sum(candidates) < target:
+            return subsets
+        can = list(set(candidates))
+        if len(can) == 1:
+            ar = [[candidates[0]] * int(target / candidates[0]) if sum(
+                [candidates[0]] * int(target / candidates[0])) == target else []]
+            return [] if len(ar[0]) == 0 else ar
+        if len(can) == 2 and len(candidates) > 10:
+            return [[can[0]] * (target - can[1]) + [can[1]], [can[0]] * target]
+
+        def combinations(i, nums, curr, all_combi, tg):
+            if i >= len(nums):
+                cur = curr.copy()
+                if sum(cur) == tg:
+                    if cur not in all_combi:
+                        all_combi.append(cur)
+                return
+            if sum(curr) < tg:
+                curr.append(nums[i])
+                combinations(i + 1, nums, curr, all_combi, tg)
+                curr.pop()
+            combinations(i + 1, nums, curr, all_combi, tg)
+
+        candidates.sort()
+        combinations(0, candidates, currset, subsets, target)
+        return subsets
 
 
 if __name__ == '__main__':
