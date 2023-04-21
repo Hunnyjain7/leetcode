638aacf8929dfa3f620159ee5d6ee688f7a3fb43
hunny <hunnyjain711@gmail.com>
2023-04-22 00:39:45 +0530

    51 update

--- 51. N-Queens.py
+++ 51. N-Queens.py
@@ -5,17 +5,40 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:  # noqa
         combs = []
         prev = ["...." for _ in range(n)]
-        prev = ["012", "345", "678"]
-        for r in range(len(prev)):
-            for c in range(r):
-                tl = prev[r - 1][c - 1]
+        prev = [
+            "012",
+            "345",
+            "678"
+        ]
+        r, c = 0, 0
+        r_end = len(prev[0])
+        c_end = len(prev)
+        while r < r_end and c < c_end:
+            tl, t, tr, ll, rr, bl, b, br = None, None, None, None, None, None, None, None
+            if (r - 1) >= 0:
+                if (c - 1) >= 0:
+                    tl = prev[r - 1][c - 1]
                 t = prev[r - 1][c]
-                ri = prev[r][c + 1]
-                le = prev[c - 1][r]
-                bl = prev[c + 1][r - 1]
-                b = prev[c + 1][r]
-                br = prev[c + 1][r + 1]
+                if (c + 1) < len(prev):
+                    tr = prev[r - 1][c + 1]
+            if (c + 1) < len(prev):
+                rr = prev[r][c + 1]
+                if (r + 1) < len(prev):
+                    br = prev[r + 1][c + 1]
+            if (c - 1) >= 0:
+                ll = prev[r][c - 1]
+            if (r + 1) < len(prev):
+                b = prev[r + 1][c]
+                if (c - 1) >= 0:
+                    bl = prev[r + 1][c - 1]
 
+            print(tl, t, tr, ll, rr, bl, b, br)
+
+            if c == c_end - 1:
+                c = 0
+                r += 1
+            else:
+                c += 1
         return combs
 
 
