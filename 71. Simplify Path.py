eebb9f3c88a969f2ba76ac2cec75f07d72e6edcf
hunny <hunnyjain711@gmail.com>
2023-05-06 21:35:35 +0530

    71 pass

--- 71. Simplify Path.py
+++ 71. Simplify Path.py
@@ -1,12 +1,27 @@
 class Solution:
-    def simplifyPath(self, path: str) -> str:
+    # Better Approach from leetcode
+    def simplifyPath(self, path: str) -> str:  # noqa
+        stack = []
+        for d in path.split('/'):
+            if d == '':
+                continue
+            elif d == '..':
+                if stack: stack.pop()
+            elif d != '.':
+                stack.append(d)
+        return '/' + '/'.join(stack)
+
+    # My solution
+    def simplifyPath2(self, path: str) -> str:  # noqa
         hashmap = {}
-        while path[-1] == "/":
+        while path and path[-1] == "/":
             path = path[:-1]
+
         while "//" in path:
             path = path.replace("//", "/")
+
         while "..." in path:
-            for p in range(0, len(path), 3):
+            for p in range(0, len(path)):
                 if path[p:p + 3] == "...":
                     dots = 0
                     while p < len(path) and path[p] == ".":
@@ -15,15 +30,53 @@ class Solution:
                     hashmap[f"{dots}__NAME__"] = dots
                     path = path.replace("." * dots, f"{dots}__NAME__")
                     break
-        # path = path[::-1]
-        while ".." in path:
+        if ".." in path.split("/"):
+            path = path.split("/")
+            path_copy = path.copy()
+            pop_next = False
+            pop_next_two = 0
+            for p in range(len(path) - 1, -1, -1):
+                if pop_next:
+                    if path[p] == ".." or path[p] == ".":
+                        if path[p] != ".":
+                            pop_next_two += 1
+                        path_copy.pop(p)
+                    else:
+                        if pop_next_two:
+                            path_copy.pop(p)
+                            pop_next_two -= 1
+                        else:
+                            # path_copy.pop(p)
+                            pop_next = False
+                elif path[p] == "..":
+                    path_copy.pop(p)
+                    pop_next_two = 1
+                    pop_next = True
+            path = "/".join(path_copy)
 
-            print(path)
-            quit()
+        if "." in path.split("/"):
+            path = path.split("/")
+            path_copy = path.copy()
+            pop_next = False
+            for p in range(len(path) - 1, -1, -1):
+                if pop_next:
+                    if path[p] == ".":
+                        path_copy.pop(p)
+                    else:
+                        pop_next = False
+                elif path[p] == ".":
+                    path_copy.pop(p)
+                    pop_next = True
+            path = "/".join(path_copy)
+
         for key, val in hashmap.items():
             path = path.replace(key, "." * val)
-        return path
+        return path if path and path[0] == "/" else "/" + path
 
 
 if __name__ == '__main__':
-    print(Solution().simplifyPath("/a/./b/../../c/"))
+    # print(Solution().simplifyPath("/a/./b/../../c/"))
+    # print(Solution().simplifyPath("/a/../../b/../c//.//"))
+    # print(Solution().simplifyPath("/a//b////c/d//././/.."))
+    print(Solution().simplifyPath("/home/foo/.ssh/../.ssh2/authorized_keys/"))
+    # print(Solution().simplifyPath("/..."))
