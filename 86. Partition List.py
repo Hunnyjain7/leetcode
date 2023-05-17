77536a90c04c55bae69a92fab7b082666e5c07e1
hunny <hunnyjain711@gmail.com>
2023-05-17 23:52:41 +0530

    86 pass

--- 86. Partition List.py
+++ 86. Partition List.py
@@ -5,33 +5,17 @@ from utils.list_node import ListNode
 
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:  # noqa
-        temp = head
-        while head and head.next:
-            if head.next.val == x:
-                print("in")
-                curr = head.next
-                last_temp = last = ListNode()
-                while curr:
-                    if curr.val < x:
-                        head.next = curr
-                        head = head.next
-                        # break
-                        print("IN IF")
-                        print("curr", curr)
-                        print("head", head)
-                        print("temp", temp)
-                    else:
-                        while last.next is not None:
-                            last = last.next
-                        last.next = ListNode(curr.val)
-                        print("IN ELSE")
-                        print("last", last)
-                        print("last_temp", last_temp)
-                        print("curr", curr)
-                        print("head", head)
-                        print("temp", temp)
-                    curr = curr.next
-                head.next = last_temp.next
-            head = head.next
+        less_head = less_tail = ListNode()
+        greater_head = greater_tail = ListNode()
 
-        return temp
+        while head:
+            if head.val < x:
+                less_tail.next = head
+                less_tail = less_tail.next
+            else:
+                greater_tail.next = head
+                greater_tail = greater_tail.next
+            head = head.next
+        greater_tail.next = None
+        less_tail.next = greater_head.next
+        return less_head.next
