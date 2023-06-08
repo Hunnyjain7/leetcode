5a8d7fcafe5e6a6452b0c649e71244dfe4a6ef76
hunny <hunnyjain711@gmail.com>
2023-06-09 00:54:36 +0530

    97 pass

--- 97. Interleaving String.py
+++ 97. Interleaving String.py
@@ -1,16 +1,28 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:  # noqa
-        # True means s1 and False means s2
-        s = True
-        s1_idx, s2_idx, s3_idx = 0, 0, 0
         s1_len, s2_len, s3_len = len(s1), len(s2), len(s3)
-        while s1_idx < s1_len and s2_idx < s2_len and s3_idx < s3_len:
-            if s:
-                pass
-            else:
-                pass
-        return s
+        if s1_len + s2_len != s3_len:
+            return False
+
+        dp = {}
+
+        def dfs(i, j):
+            if i == s1_len and j == s2_len:
+                return True
+
+            if (i, j) in dp:
+                return dp[(i, j)]
+
+            if i < s1_len and s3[i + j] == s1[i] and dfs(i + 1, j):
+                return True
+
+            if j < s2_len and s3[i + j] == s2[j] and dfs(i, j + 1):
+                return True
+
+            dp[(i, j)] = False
+
+        return dfs(0, 0)
 
 
 if __name__ == '__main__':
-    print(Solution().isInterleave("aabcc", "dbbca", "aadbbcbcac"))
+    print(Solution().isInterleave("aadbcc", "dbbca", "aadbbcdbcac"))
