7b60436f7072e2fe8ad07a67e2d72fd7918ec47f
hunny <hunnyjain711@gmail.com>
2023-06-22 23:39:29 +0530

    110 pass

--- 110. Balanced Binary Tree.py
+++ 110. Balanced Binary Tree.py
@@ -4,5 +4,13 @@ from utils.tree_node import TreeNode
 
 
 class Solution:
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        pass
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:  # noqa
+        def dfs(root_node):
+            if not root_node:
+                return 0
+            left, right = dfs(root_node.left), dfs(root_node.right)
+            if left < 0 or right < 0 or abs(left - right) > 1:
+                return -1
+            return max(left, right) + 1
+
+        return dfs(root) >= 0
