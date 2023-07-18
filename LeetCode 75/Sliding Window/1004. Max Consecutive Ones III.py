fe04f044a337d5cf98ee8a618db26b1fc4b90549
hunny <hunnyjain711@gmail.com>
2023-07-19 00:26:37 +0530

    LeetCode 75: (Sliding Window) 1004 pass

--- LeetCode 75/Sliding Window/1004. Max Consecutive Ones III.py
+++ LeetCode 75/Sliding Window/1004. Max Consecutive Ones III.py
@@ -24,8 +24,26 @@ from typing import List
 
 
 class Solution:
-    def longestOnes(self, nums: List[int], k: int) -> int:
-        pass
+    def longestOnes(self, nums: List[int], k: int) -> int:  # noqa
+        left = right = 0
+
+        for right in range(len(nums)):
+            # if we encounter a 0 the we decrement K
+            if nums[right] == 0:
+                k -= 1
+            # else no impact to K
+
+            # if K < 0 then we need to move the left part of the window forward
+            # to try and remove the extra 0's
+            if k < 0:
+                # if the left one was zero then we adjust K
+                if nums[left] == 0:
+                    k += 1
+                # regardless of whether we had a 1 or a 0 we can move left side by 1
+                # if we keep seeing 1's the window still keeps moving as-is
+                left += 1
+
+        return right - left + 1
 
 
 if __name__ == '__main__':
