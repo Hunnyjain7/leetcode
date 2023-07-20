011814741ae89e810b05ad36bfcb0fe213aa3d95
hunny <hunnyjain711@gmail.com>
2023-07-21 01:04:22 +0530

    LeetCode 75: (Sliding Window) 1493 pass (Prefix Sum) 1732 pass

---
+++ LeetCode 75/Sliding Window/1493. Longest Subarray of 1's After Deleting One Element.py
@@ -0,0 +1,49 @@
+"""Given a binary array nums, you should delete one element from it.
+
+Return the size of the longest non-empty subarray containing only 1's in the resulting array.
+Return 0 if there is no such subarray.
+
+Example 1:
+
+Input: nums = [1,1,0,1]
+Output: 3
+Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+
+Example 2:
+
+Input: nums = [0,1,1,1,0,1,1,0,1]
+Output: 5
+Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with
+value of 1's is [1,1,1,1,1].
+
+Example 3:
+
+Input: nums = [1,1,1]
+Output: 2
+Explanation: You must delete one element.
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1."""
+from typing import List
+
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        left = right = 0
+
+        k = 1
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                k -= 1
+
+            if k < 0:
+                if nums[left] == 0:
+                    k += 1
+                left += 1
+
+        return right - left
+
+if __name__ == '__main__':
+    print(Solution().longestSubarray([1,0,1,0,1,0]))
